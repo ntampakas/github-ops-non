60d7772b2b579a4b8a19040c4143e83ff9732383
Nick Tampakas <nick@gmx.co.uk>
2022-12-13 15:10:22 +0200

    Dev

--- run.sh
+++ run.sh
@@ -19,19 +19,18 @@ for WORKFLOW_ID in $QUEUED; do
   TAG="${REPO}-${WORKFLOW_ID}"
   RUNNER_LABELS=$(echo "${JOB_LABELS}" | jq -cr 'join(",")')
 
-    # continue on error
-    cat cloud-init.sh | sed -e "s#__REPO__#${REPO}#" -e "s/__RUNNER_LABELS__/${RUNNER_LABELS}/" -e "s/__GITHUB_TOKEN__/${GH_PAT}/" > .startup.sh
-    aws ec2 run-instances \
-      --user-data "file://.startup.sh" \
-      --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 64, \"DeleteOnTermination\": true } } ]" \
-      --ebs-optimized \
-      --instance-initiated-shutdown-behavior terminate \
-      --instance-type "${INSTANCE_TYPE}" \
-      --image-id "${IMAGE_ID}" \
-      --key-name "${KEY_NAME}" \
-      --subnet-id "${SUBNET_ID}" \
-      --security-group-id "${SECURITY_GROUP_ID}" \
-      --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${TAG}}]" || EXIT_CODE=1
+  cat cloud-init.sh | sed -e "s#__REPO__#${REPO}#" -e "s/__RUNNER_LABELS__/${RUNNER_LABELS}/" -e "s/__GITHUB_TOKEN__/${GH_PAT}/" > .startup.sh
+  aws ec2 run-instances \
+    --user-data "file://.startup.sh" \
+    --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 64, \"DeleteOnTermination\": true } } ]" \
+    --ebs-optimized \
+    --instance-initiated-shutdown-behavior terminate \
+    --instance-type "${INSTANCE_TYPE}" \
+    --image-id "${IMAGE_ID}" \
+    --key-name "${KEY_NAME}" \
+    --subnet-id "${SUBNET_ID}" \
+    --security-group-id "${SECURITY_GROUP_ID}" \
+    --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${TAG}}]" || EXIT_CODE=1
 done
 
 exit "${EXIT_CODE}"
