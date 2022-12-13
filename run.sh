54e5a5c65c804a77c25aa2b6189141f14bd3102c
Nick Tampakas <nick@gmx.co.uk>
2022-12-13 14:12:17 +0200

    Dev

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ set -eux
 
 EXIT_CODE=0
 QUEUED=$(curl -H "authorization: token ${GH_PAT}" "https://api.github.com/repos/${REPO}/actions/runs?status=queued" | jq -cr '.workflow_runs[].id')
-QUEUED=3684689819
 for WORKFLOW_ID in $QUEUED; do
   JOB_DATA=$(curl -H "authorization: token ${GH_PAT}" "https://api.github.com/repos/${REPO}/actions/runs/${WORKFLOW_ID}/jobs" | jq -cr '.')
 
